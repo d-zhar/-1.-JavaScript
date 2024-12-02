0c1d55d743b981c83825aea49c1331671cf91159
Artem <150905736+d-zhar@users.noreply.github.com>
2024-12-02 21:28:55 +0300

    add syntax

---
+++ syntax/variables/index.js
@@ -0,0 +1,11 @@
+let myName = "Artem";
+const mySurname = "Singur";
+let myFavoriteDrink = "water";
+const myFavoriteAnimal = "dog";
+let myFavoriteProgrammingLanguage = "JS";
+
+console.log(myName);
+console.log(mySurname);
+console.log(myFavoriteDrink);
+console.log(myFavoriteAnimal);
+console.log(myFavoriteProgrammingLanguage);
---
+++ syntax/variables2/car.js
@@ -0,0 +1,25 @@
+const carName = "nissan";
+
+let maxCarSpeed = "220";
+
+let carOwner = "Artem";
+
+alert(carName);
+alert(maxCarSpeed);
+alert(carOwner);
+
+// speed
+maxCarSpeed = "240";
+console.log("У машины изменилась скорость", maxCarSpeed);
+maxCarSpeed = "260";
+console.log("У машины изменилась скорость", maxCarSpeed);
+maxCarSpeed = "280";
+console.log("У машины изменилась скорость", maxCarSpeed);
+
+// owner
+carOwner = "Ivan";
+console.log("У машины изменился владелец", carOwner);
+carOwner = "Dima";
+console.log("У машины изменился владелец", carOwner);
+carOwner = "Maksim";
+console.log("У машины изменился владелец", carOwner);
